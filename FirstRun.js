2db33722c878338a6c0df732a6d7a5bd25172b36
Copulatrix <maher.chris@outlook.com>
2020-03-08 21:06:08 +1100

    init

--- FirstRun.js
+++ FirstRun.js
@@ -23,7 +23,7 @@ fs.writeFileSync('/etc/rc.local', `#!/bin/sh -e
 #
 # By default this script does nothing.
 
-echo '' > /Slave/Log1.log; forever /Slave/Start.js |& tee -a /Slave/Log1.log & 
+forever /Slave/Start.js & 
 
 exit 0`);
 console.log(execSync('chmod +x /etc/rc.local').toString());
