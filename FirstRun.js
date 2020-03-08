ad22a70ab7b041ab06e0bff5b3ad8bf84a641923
Copulatrix <maher.chris@outlook.com>
2020-03-08 20:59:32 +1100

    init

--- FirstRun.js
+++ FirstRun.js
@@ -11,12 +11,22 @@ var request = require('request');
 
 if(!dev){
 fs.writeFileSync('/etc/rc.local', `#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
 
-echo '' > /Slave/Log1.log;
-forever /Slave/Start.js |& tee -a /Slave/Log1.log & 
+echo '' > /Slave/Log1.log; forever /Slave/Start.js |& tee -a /Slave/Log1.log & 
 
 exit 0`);
-execSync('chmod +x /etc/rc.local').toString();
+console.log(execSync('chmod +x /etc/rc.local').toString());
 }
 request({
     method: 'POST',
